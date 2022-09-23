797c85d5ce2baf7a071df9c4e90854d4ab211778
Luis Imagiire <luishenrique.imagiire@gmail.com>
2022-09-23 16:32:22 -0300

    ebm pair-wise working and comparisson to interpretml

--- core/ebm_mini.py
+++ core/ebm_mini.py
@@ -1,13 +1,14 @@
-# TODO: Implement EBM Mini
 from core.decision_tree_mini import DT
 import numpy as np
 import pandas as pd
 from core.utils import rmse, make_bulk_prediction
 from tqdm import tqdm
+import itertools
+from typing import Optional
 
 
 class EBM:
-    def __init__(self, num_iter=10, max_tree_size=3, learning_rate=0.01):
+    def __init__(self, num_iter: int = 10, max_tree_size: Optional[int] = 3, learning_rate: float = 0.01):
         self.M = num_iter
         self.max_node = max_tree_size
         self.lr = learning_rate
@@ -15,18 +16,40 @@ class EBM:
         self._y_mu = 0
 
     def fit(self, X, y):
-        feats_num = X.shape[1]
+        feats_num = [str(c) for c in range(X.shape[1])]
         self._y_mu = np.mean(y)
-        self.model = {c: [] for c in range(feats_num)}
+        self.model = {c: [] for c in feats_num}
         y_target = y - self._y_mu
         for m in tqdm(range(self.M)):
-            for feat in range(feats_num):
+            for feat in feats_num:
+                _m_feat = int(feat) if "_" not in feat else [int(c) for c in feat.split('_')]
                 model = DT(minimum_obs=1, maximum_tree_size=self.max_node)
-                model = model.fit(X[:, feat].reshape(-1, 1), y_target)
-                preds = make_bulk_prediction(X[:, feat], model)
+                if isinstance(_m_feat, int):
+                    model = model.fit(X[:, _m_feat].reshape(-1, 1), y_target)
+                else:
+                    model = model.fit(X[:, _m_feat], y_target)
+                preds = make_bulk_prediction(X[:, _m_feat], model)
                 model_predictions = self.predict_bulk(X)
                 y_target = y - (model_predictions + self.lr * preds)
                 self.model[feat].append(model)
+
+            if m == 0:
+                # Find pair-wise interactions
+                # Original EBM uses FAST algorithm to select suitable pair-wise interaction
+                # Out of scope for us since that is mainly to control the computational cost of fitting pair-wise models
+                feature_ss = list(itertools.combinations(feats_num, 2))
+                model_predictions = self.predict_bulk(X)
+                base_rss = np.sum(y - model_predictions)
+                for pair in feature_ss:
+                    _idx_pair = [idx for idx in map(int, pair)]
+                    model = DT(minimum_obs=1, maximum_tree_size=self.max_node)
+                    model = model.fit(X[:, _idx_pair], y_target)
+                    preds = make_bulk_prediction(X[:, _idx_pair], model)
+                    iter_resids = y - (model_predictions + self.lr * preds)
+                    if np.sum(iter_resids) < base_rss:
+                        m_tag = "_".join(pair)
+                        self.model[m_tag] = [model]
+                        feats_num.append(m_tag)
 
             print(f"RUN {m} - RMSE {rmse(y, self.predict_bulk(X))}")
         return self
@@ -41,7 +64,10 @@ class EBM:
         prediction_map = {c: 0 for c in self.model.keys()}
         for feat, models in self.model.items():
             for m in models:
-                prediction_map[feat] += self.lr * DT.predict(m, X.reshape(-1, 1)[feat])
+                if "_" not in feat:
+                    prediction_map[feat] += self.lr * DT.predict(m, X.reshape(-1, 1)[int(feat)])
+                else:
+                    prediction_map[feat] += self.lr * DT.predict(m, X[[int(c) for c in feat.split("_")]])
 
         return prediction_map if return_pred_map else self._y_mu + np.sum(list(prediction_map.values()))
 
@@ -60,8 +86,8 @@ def _test():
     X_test = df[_feats].values[train_num::]
 
     # Train!
-    _minobs = 100
-    test = EBM(num_iter=_minobs, max_tree_size=32, learning_rate=0.1)
+    _minobs = 5
+    test = EBM(num_iter=_minobs, max_tree_size=None, learning_rate=0.5)
     test = test.fit(X, y)
 
     pred_test = test.predict_bulk(X_test)
