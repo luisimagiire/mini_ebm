efe3df92a5137986bffbadd2a4b85f083a24fe4c
Luis Imagiire <luishenrique.imagiire@gmail.com>
2022-09-23 09:35:07 -0300

    ebm fixed, we are learning

--- core/ebm_mini.py
+++ core/ebm_mini.py
@@ -3,6 +3,7 @@ from core.decision_tree_mini import DT
 import numpy as np
 import pandas as pd
 from core.utils import rmse, make_bulk_prediction
+from tqdm import tqdm
 
 
 class EBM:
@@ -16,17 +17,22 @@ class EBM:
     def fit(self, X, y):
         feats_num = X.shape[1]
         self._y_mu = np.mean(y)
-        models_map = {c: [] for c in range(feats_num)}
+        self.model = {c: [] for c in range(feats_num)}
         y_target = y - self._y_mu
-        for m in range(self.M):
+        for m in tqdm(range(self.M)):
             for feat in range(feats_num):
                 model = DT(minimum_obs=1, maximum_tree_size=self.max_node)
                 model = model.fit(X[:, feat].reshape(-1, 1), y_target)
-                models_map[feat].append(model)
                 preds = make_bulk_prediction(X[:, feat], model)
-                y_target = y_target - self.lr * preds
-        self.model = models_map
+                model_predictions = self.predict_bulk(X)
+                y_target = y - (model_predictions + self.lr * preds)
+                self.model[feat].append(model)
+
+            print(f"RUN {m} - RMSE {rmse(y, self.predict_bulk(X))}")
         return self
+
+    def predict_bulk(self, X):
+        return np.array([self.predict(np.asarray(X)[i, :]) for i in range(X.shape[0])])
 
     def predict(self, X, return_pred_map=False):
         if self.model is None:
@@ -35,7 +41,7 @@ class EBM:
         prediction_map = {c: 0 for c in self.model.keys()}
         for feat, models in self.model.items():
             for m in models:
-                prediction_map[feat] += self.lr * DT.predict(m, X)
+                prediction_map[feat] += self.lr * DT.predict(m, X.reshape(-1, 1)[feat])
 
         return prediction_map if return_pred_map else self._y_mu + np.sum(list(prediction_map.values()))
 
@@ -54,13 +60,12 @@ def _test():
     X_test = df[_feats].values[train_num::]
 
     # Train!
-    _minobs = 10
-    test = EBM(num_iter=_minobs, max_tree_size=None, learning_rate=0.1)
+    _minobs = 100
+    test = EBM(num_iter=_minobs, max_tree_size=32, learning_rate=0.1)
     test = test.fit(X, y)
 
-    pred_ebm = lambda x: test.predict(x, return_pred_map=False)
-    pred_test = np.array([pred_ebm(np.asarray(X_test.reshape(-1, 1))[i, :]) for i in range(X_test.shape[0])])
-    pred_train = np.array([pred_ebm(np.asarray(X.reshape(-1, 1))[i, :]) for i in range(X.shape[0])])
+    pred_test = test.predict_bulk(X_test)
+    pred_train = test.predict_bulk(X)
 
     print(f"TRAIN RMSE = {rmse(y, pred_train)}")
     print(f"TEST RMSE = {rmse(y_test, pred_test)}")
